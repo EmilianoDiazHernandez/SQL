94302f521191dc5819b7d7241182abde39186ca2
Emiliano <ediazh1900@alumno.ipn.mx>
2024-09-14 10:01:59 -0600

    change UI

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    static boolean error = false;
+
+    public static void main(String[] args) throws IOException {
+        Prompt();
+    }
+
+    private static void Prompt() throws IOException{
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        while(true){
+            System.out.print(">>");
+            String statement = reader.readLine();
+            if(statement == null) break; // Ctrl + D
+            ejecutar(statement);
+            error = false;
+        }
+    }
+
+    private static void ejecutar(String source){
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for(Token token : tokens){
+            System.out.println(token);
+        }
+    }
+
+    /*
+    El método error se puede usar desde las distintas clases
+    para reportar los errores:
+    Interprete.error(....);
+     */
+    static void error(int linea, String mensaje){
+        reportar(linea, "", mensaje);
+    }
+
+    private static void reportar(int linea, String donde, String mensaje){
+        System.err.println(
+                "[linea " + linea + "] Error " + donde + ": " + mensaje
+        );
+        error = true;
+    }
+}
